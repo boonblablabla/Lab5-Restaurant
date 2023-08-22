5d80a6bae3856d5b529d18b5f26d5af1e89a26d3
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-08-22 17:20:43 +0700

    Add function 'getByName' and 'getByLocation' to Controller

--- src/main/java/ku/cs/restaurant/controller/RestaurantController.java
+++ src/main/java/ku/cs/restaurant/controller/RestaurantController.java
@@ -37,4 +37,14 @@ public class RestaurantController {
     public Restaurant delete(@PathVariable UUID id) {
         return service.delete(id);
     }
+
+    @GetMapping("/restaurant/name/{name}")
+    public Restaurant getRestaurantByName(@PathVariable String name) {
+        return service.getRestaurantByName(name);
+    }
+
+    @GetMapping("/restaurant/location/{location}")
+    public List<Restaurant> getRestaurantByLocation(@PathVariable String location) {
+        return service.getRestaurantByLocation(location);
+    }
 }
